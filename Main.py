ad87d21ebbb62acda11e8df9d63af56b2ba00a02
Edt12 <104518243+Edt12@users.noreply.github.com>
2022-11-23 15:03:33 +0000

    First Commit added Login Screen

---
+++ Main.py
@@ -0,0 +1,88 @@
+import sqlite3
+from kivy.uix.screenmanager import ScreenManager, Screen,NoTransition #Kivy has screens not pop up windows so screen manage manager different screens think of like switching between virtual desktops
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label#import widgets such as buttons and labels 
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.lang import Builder
+from kivy.core.window import Window
+sm=ScreenManager()
+green = [0, 1, 0, 1] #RGBA values /255
+Grey=[0.8,0.8,0.8,1]
+Black=[0,0,0,1]
+#Creating Sqlite Database
+conn=sqlite3.connect("UsersAndPasswords.db")#connects to database 
+cursor=conn.cursor()#adds connection to cursor
+#creates SQlite Database
+cursor.execute("""create table IF NOT EXISTS UsersAndPasswords 
+(Username text
+,Password text 
+)""")#inside are columns/categorys
+
+class Login(Screen):#Create different windows class
+    
+    def __init__(self,**kwargs):#Instead of using build to intialise use init as build does not work with screen class
+        Screen.__init__(self,**kwargs)
+    
+        Window.clearcolor =(Grey)#sets background color for window to get value take each rgb value and divide by 255
+
+        self.layout=FloatLayout()#float layout allows you to place widgets anywhere
+
+        Username=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.5})#creates Input box called username,on size_hint first value is length second is width
+        self.add_widget(Username)
+
+        Password=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.4})#creates Input box called username,on size_hint first value is length second is width
+        self.add_widget(Password)
+
+        EnterUsernameandPassword=Button(size_hint=(0.1,0.05),pos_hint={'x':0.7,'y':0.5},text="Enter",background_color=green)
+
+   
+        def LoginClick(self):
+            cursor.execute("SELECT * From UsersAndPasswords")
+            Table=cursor.fetchall()
+            username=cursor.execute("SELECT Username From UsersAndPasswords")
+            
+
+            for row in Table:#goes through every column in UserAndPasswords
+                if row[0]== Username.text and row[1]== Password.text:
+                    print("Both Correct")
+                    sm.current="PasswordMenu"
+
+                if row[0]== Username.text and row[1]!= Password.text:
+                    Password.text==""
+                    print("incorrect password")
+
+                if row[0]!= Username.text and row[1]== Password.text:
+                    Username.text==""
+                    print("incorrect username")
+                    
+            
+
+        EnterUsernameandPassword.bind(on_press=LoginClick)
+        self.add_widget(EnterUsernameandPassword)
+
+        LoginTitle=Label(text="Please Enter Your Username and Password",size_hint=(0.1,0.05),pos_hint={'x':0.5,'y':0.6},color=Black)
+        self.add_widget(LoginTitle)
+        
+        def CreatePasswordClick(self):
+            if Username.text != "" and Password.text!="":
+                testUsername="test"
+                testPassword="1234"
+                cursor.execute("INSERT INTO UsersAndPasswords(Username,Password) VALUES (?,?)",(testUsername,testPassword))
+                Username.text=""
+                Password.text=""
+        CreatePassword=Button(size_hint=(0.2,0.05),pos_hint={'x':0.7,'y':0.4},text="Create New Password",background_color=green)
+        CreatePassword.bind(on_press=CreatePasswordClick)
+        self.add_widget(CreatePassword)
+
+
+class PaperApp(App):
+    def build(self):
+        sm.add_widget(Login(name="Login"))
+        sm.current="Login"
+        return sm
+
+PaperApp().run()
