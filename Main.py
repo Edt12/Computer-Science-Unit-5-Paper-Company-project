9b7a7a71a68f2cd228eb3b1f07b46b3024f1031e
Edt12 <edtickle@icloud.com>
2023-02-26 16:49:06 +0000

    changes

--- Main.py
+++ Main.py
@@ -9,15 +9,224 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
+import os
+import base64
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.fernet import Fernet
+import hashlib
 
 green = [0, 1, 0, 1] #RGBA values /255 
 Grey=[0.8,0.8,0.8,1]
 Black=[0,0,0,1]
+KeyStore=[]
 #Note this protype currently only covers the customer user type in future manager and employee functions will be added
 #Creating Sqlite Database
 conn=sqlite3.connect("DunderMifflinDatabase.db")#connects to database 
 cursor=conn.cursor()#adds connection to cursor
 Screenmanager=ScreenManager()#Each Screen is called by screen manager which is used for commands which involve changing between screens
+#generates encryption key using Scrypt
+def GenerateKey(UsernameAndPassword,salt):
+    KeyDerivationFunction=Scrypt(salt=salt,length=32,n=2**20,r=8,p=1)
+    UsernameAndPassword=str(UsernameAndPassword).encode()   
+    Key=base64.urlsafe_b64encode(KeyDerivationFunction.derive(UsernameAndPassword))
+    cipher=Fernet(Key)
+    return cipher
+
+def Encrypt(Data):
+     pass
+def Decrypt(Data):
+     pass
+
+
+       
+        
+class Login(Screen):
+   
+    def __init__(self,**kwargs):
+        Screen.__init__(self,**kwargs)
+    
+        Window.clearcolor =(Grey)#sets background color for window to get value take each rgb value and divide by 255
+
+        self.layout=FloatLayout()
+
+        UsernameEntry=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.5})
+        self.add_widget(UsernameEntry)
+
+        PasswordEntry=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.4})
+        self.add_widget(PasswordEntry)
+
+        EnterUsernameandPassword=Button(size_hint=(0.1,0.05),pos_hint={'x':0.7,'y':0.5},text="Enter",background_color=green)
+
+
+        def LoginClick(self):
+           
+            
+            ProductPos_hintX=0.0
+            ProductPos_hintY=0.0
+            ProductNumber=0
+            #hashes Username
+            Username=UsernameEntry.text
+            EncodedUsername=Username.encode()
+            HashedUsername=hashlib.sha3_512(EncodedUsername)
+            #hashes password
+            Password=PasswordEntry.text
+            EncodedPassword=Password.encode()
+            HashedPassword=hashlib.sha3_512(EncodedPassword)
+            cursor.execute("SELECT * From UsersAndPasswords")
+            Table=cursor.fetchall()
+            if Table!=[]:
+                for row in Table:#goes through every row in UserAndPasswords Table
+                    #compare inputed hashed username and password when they are in hexdigest form as that is how they are stored in database
+                    print("compare")
+                
+                    if row[1]== HashedUsername.hexdigest() and row[2]== HashedPassword.hexdigest():#rows in sqlite can be referenced through a list e.g row 0 is first column ,row 1 is second column
+                        print("Both Correct")#Username.text and Password.text are text from login input boxes
+                        #generate cipher for encryption
+                        HexedHashedUsername=HashedUsername.hexdigest()
+                        cursor.execute("Select Salt from UsersAndPasswords where Username=(?)",(HexedHashedUsername,))
+                        Salt=cursor.fetchone()
+                        UsernameAndPassword=HashedPassword.hexdigest()+HashedUsername.hexdigest()
+                        cipher=GenerateKey(UsernameAndPassword,salt=Salt[0])
+                        KeyStore.append(cipher)
+                        #defines encrypt and decrypt functions
+                        def Decrypt(Data):
+                            DecryptedData=cipher.decrypt(Data)
+                            OriginalData=DecryptedData.decode()
+                            return OriginalData
+                        
+                        def Encrypt(Data):
+                            Data=str(Data).encode()
+                            EncryptedData=cipher.encrypt(Data)
+                            return EncryptedData
+                        
+                        cursor.execute("SELECT * from Products")
+                        Table=cursor.fetchall()
+                        print(Table)
+                        for row in Table: 
+                            Screenmanager.current="Shopfront"
+                            print("steve")
+                            ShopfrontScreen=Screenmanager.get_screen("Shopfront")#get screen grabs an instance of a screen and is used to place widgets on different screens
+
+                            ProductName=row[0]
+                            ProductPrice=row[1]
+                            cursor.execute("SELECT * from Products")
+                        
+                        
+
+                            def ProductPress(self):
+                                Quantity=0
+                                ProductPressed=self.text
+                                Quantity+=1
+
+                                
+                                cursor.execute("SELECT *FROM Basket Where Productname = (?)",(ProductPressed,))#First Searches for item in basket
+                                Product=self.text#getting title then using it to search the database for its price then adding price to basket
+                                cursor.execute("SELECT *FROM Products Where Productname = (?)",(Product,))
+                                ProductPrice=row[1]
+                               
+                                EncryptedProduct=Encrypt(Product)
+                                cursor.execute("Select *from Basket where Productname=(?)",(EncryptedProduct,))
+                                InBasket=cursor.fetchall()
+                                if InBasket !=[]:
+                                     cursor.execute("Select Quantity from Basket where Productname=(?)",(EncryptedProduct,))
+                                     TempQuantity=cursor.fetchall()
+                                     Quantity=TempQuantity[0]
+                                     Quantity+=1
+                                     cursor.execute("Update Basket Set Quantity=? Where Productname=?",(Quantity,EncryptedProduct))
+                                     conn.commit()
+
+                                else:
+                                    EncryptedProductPrice=Encrypt(ProductPrice)
+                                    EncryptedQuantity=Encrypt(Quantity)
+                                    cursor.execute("INSERT INTO Basket(Productname,ProductPrice,Quantity)VALUES(?,?,?)",(EncryptedProduct,EncryptedProductPrice,EncryptedQuantity))
+                                    conn.commit()
+
+ 
+                        IndividualProduct=Button(size_hint=(0.2,0.1),pos_hint={'x':ProductPos_hintX,'y':ProductPos_hintY},text=str(ProductName),background_color=green,color=Black)
+                        IndividualProduct.bind(on_press=ProductPress)
+                        ShopfrontScreen.add_widget(IndividualProduct)
+                        ProductPos_hintX+=0.2
+                        ProductNumber+=1
+
+                        ProductNumber_DividedBy5=ProductNumber/5
+                        if ProductNumber_DividedBy5.is_integer():#is integer checks whether something is integer
+                            ProductPos_hintX=0
+                            ProductPos_hintY+=0.1
+                    
+            if row[0]== UsernameEntry.text and row[1]!= PasswordEntry.text:
+                        print("incorrect")
+                        
+            if row[0]!= UsernameEntry.text and row[1]== PasswordEntry.text:
+                        print("incorrect")
+
+            if row[0]!= UsernameEntry.text and row[1]== PasswordEntry.text:
+                        print("incorrect")
+                   
+        EnterUsernameandPassword.bind(on_press=LoginClick)
+        self.add_widget(EnterUsernameandPassword)
+
+        AddNewCustomer=Button(size_hint=(0.2,0.05),pos_hint={'x':0.7,'y':0.4},text=str("Add New Customer"),background_color=green)
+        def AddCustomer(self):
+            CustomerUsername=UsernameEntry.text
+            CustomerPassword=PasswordEntry.text
+            EncodedUsername=CustomerUsername.encode()#To hash first encode then hash
+            HashedUsername=hashlib.sha3_512(EncodedUsername)
+            EncodedPassword=CustomerPassword.encode()
+            HashedPassword=hashlib.sha3_512(EncodedPassword)
+            UserType="Customer"#all accounts made this way will be customer level access as higher levels only made by managers
+            cursor.execute("Select Username from UsersAndPasswords")
+            Usernames=cursor.fetchall()
+            print(Usernames)
+            def check():
+                UsernameAlreadyUsed=False
+                if Usernames!=[]:
+                    for row in Usernames:
+                        print("rowing")
+                        if HashedUsername.hexdigest()==row[0]:
+                            print("Checking")
+                            UsernameAlreadyUse=True
+                            return UsernameAlreadyUse
+                        else:
+                             return UsernameAlreadyUse
+                else: 
+                    return UsernameAlreadyUsed       
+                        
+            UsernameAlreadyUse=check()#function so commands below arent executed in a loop
+            print(UsernameAlreadyUse)
+            #Insert hashes into database as hex digest as by defualt their datatype is not supported
+            if UsernameAlreadyUse==False:
+                salt=os.urandom(32)
+                cursor.execute("Insert into UsersAndPasswords (Username,Password,UserType,Salt)VALUES(?,?,?,?)",(HashedUsername.hexdigest(),HashedPassword.hexdigest(),UserType,salt))
+                conn.commit()
+                print("Adding")
+            if UsernameAlreadyUse==True:
+                UsernameEntry.text="Username Already Used"
+                print("Not Adding")
+            
+            UsernameEntry.text=""
+            PasswordEntry.text=""
+        AddNewCustomer.bind(on_press=AddCustomer)
+        self.add_widget(AddNewCustomer)
+        LoginTitle=Label(text="Please Enter Your Username and Password",size_hint=(0.1,0.05),pos_hint={'x':0.5,'y':0.6},color=Black)
+        self.add_widget(LoginTitle)
+
+
+def Decrypt(Data):
+    cipher=KeyStore[0]
+    print(cipher)
+    DecryptedData=cipher.decrypt(Data)
+    OriginalData=DecryptedData.decode()
+    return OriginalData
+                        
+def Encrypt(Data):
+    cipher=KeyStore[0]
+    print(cipher)
+    Data=str(Data).encode()
+    EncryptedData=cipher.encrypt(Data)
+    return EncryptedData
+                        
+#inputs-Payment Information
+#outputs-Order(deliveryaddress and Order)
 class PaymentScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -118,9 +327,11 @@ class Shopfront(Screen):
             ViewBasketScreen=Screenmanager.get_screen("ViewBasket")
             #In future need to add some way of getting the quantity of a specific item and removing the previous label if more items are added to basket after someone has already pressed viewbasket
             cursor.execute("Select * From Basket")
-            Table=cursor.fetchall()
-            Product=Label(text=str(Table),size_hint=(0.2,0.1),pos_hint={'x':0.5,'y':0.5},color=Black)
-            ViewBasketScreen.add_widget(Product)
+            Basket=cursor.fetchall()
+            for row in Basket:
+                DecryptedRow=Decrypt(row)
+                Product=Label(text=DecryptedRow,size_hint=(0.2,0.1),pos_hint={'x':0.5,'y':0.5},color=Black)
+                ViewBasketScreen.add_widget(Product)
             
         
         Viewbasket=Button(size_hint=(0.2,0.1),pos_hint={'x':0.8,'y':0.9},text="ViewBasket",background_color=green,color=Black)
@@ -129,135 +340,51 @@ class Shopfront(Screen):
 
             #help button will be added later
 
-
-       
-        
-class Login(Screen):
-   
-    def __init__(self,**kwargs):
-        Screen.__init__(self,**kwargs)
-    
-        Window.clearcolor =(Grey)#sets background color for window to get value take each rgb value and divide by 255
-
-        self.layout=FloatLayout()
-
-        Username=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.5})
-        self.add_widget(Username)
-
-        Password=TextInput(size_hint=(0.2,0.05),pos_hint={'x':0.4,'y':0.4})
-        self.add_widget(Password)
-
-        EnterUsernameandPassword=Button(size_hint=(0.1,0.05),pos_hint={'x':0.7,'y':0.5},text="Enter",background_color=green)
-
-   
-        def LoginClick(self):
-            cursor.execute("SELECT * From UsersAndPasswords")
-            Table=cursor.fetchall()
-            username=cursor.execute("SELECT Username From UsersAndPasswords")
-            
-            ProductPos_hintX=0.0
-            ProductPos_hintY=0.0
-            ProductNumber=0
-          
-            for row in Table:#goes through every row in UserAndPasswords Table
-                if row[0]== Username.text and row[1]== Password.text:#rows in sqlite can be referenced through a list e.g row 0 is first column ,row 1 is second column
-                    print("Both Correct")#Username.text and Password.text are text from login input boxes
-                    cursor.execute("SELECT * from Products")
-                    Table=cursor.fetchall()
-                    print(Table)
-                    for row in Table: 
-                        Screenmanager.current="Shopfront"
-                        print("steve")
-                        ShopfrontScreen=Screenmanager.get_screen("Shopfront")#get screen grabs an instance of a screen and is used to place widgets on different screens
-
-                        ProductName=row[0]
-                        ProductPrice=row[1]
-                        cursor.execute("SELECT * from Products")
-                    
-                        print(ProductName)
-                        print(cursor.fetchall())
-
-                        def ProductPress(self):
-                            Quantity=0
-                            ProductPressed=self.text
-                            Quantity+=1
-                            cursor.execute("SELECT *FROM Basket Where Productname = (?)",(ProductPressed,))#First Searches for item in basket
-                            Product=self.text#getting title then using it to search the database for its price then adding price to basket
-                            cursor.execute("SELECT *FROM Products Where Productname = (?)",(Product,))
-                            ProductPrice=row[1]
-                            print(ProductPrice)
-                            cursor.execute("INSERT INTO Basket(Productname,ProductPrice,Quantity)VALUES(?,?,?)",(Product,ProductPrice,Quantity))
-                            conn.commit()
-                            cursor.execute("SELECT * FROM Basket")
-                            cursor.fetchall()
-                            
-
- 
-                        IndividualProduct=Button(size_hint=(0.2,0.1),pos_hint={'x':ProductPos_hintX,'y':ProductPos_hintY},text=str(ProductName),background_color=green,color=Black)
-                        IndividualProduct.bind(on_press=ProductPress)
-                        ShopfrontScreen.add_widget(IndividualProduct)
-                        ProductPos_hintX+=0.2
-                        ProductNumber+=1
-
-                        ProductNumber_DividedBy5=ProductNumber/5
-                        if ProductNumber_DividedBy5.is_integer():#is integer checks whether something is integer
-                            ProductPos_hintX=0
-                            ProductPos_hintY+=0.1
-                    
-            if row[0]== Username.text and row[1]!= Password.text:
-                        print("incorrect")
-                        
-            if row[0]!= Username.text and row[1]== Password.text:
-                        print("incorrect")
-
-            if row[0]!= Username.text and row[1]== Password.text:
-                        print("incorrect")
-                        
-            
-
-        EnterUsernameandPassword.bind(on_press=LoginClick)
-        self.add_widget(EnterUsernameandPassword)
-
-        LoginTitle=Label(text="Please Enter Your Username and Password",size_hint=(0.1,0.05),pos_hint={'x':0.5,'y':0.6},color=Black)
-        self.add_widget(LoginTitle)
-        
-    
-
-
-
 def main():
+  
     #payment information table will be added in future
     cursor.execute("""create table IF NOT EXISTS Products(
-    Productname text
-    ,ProductPrice text
+    Productname text Primary Key
+    ,ProductPrice text(6)
     )""")
 
 
     cursor.execute("""create table IF NOT EXISTS Basket(
-    Productname text
-    ,ProductPrice text
-    ,Quantity text
+    Productname blob(128)
+    ,ProductPrice blob(128)
+    ,Quantity blob (128)
     )""")
 
     #creates SQlite Database
-    cursor.execute("""create table IF NOT EXISTS UsersAndPasswords 
-    (Username text
-    ,Password text 
+    cursor.execute("""create table IF NOT EXISTS UsersAndPasswords
+    (UserID integer Primary Key Autoincrement
+    ,Username blob (512) 
+    ,Password blob (512)
+    ,Salt int(32)
+    ,Usertype text
     )""")#inside are columns/categorys
+
+    cursor.execute("""create Table IF NOT EXISTS PaymentInfo(
+     CardNumber blob(128) Primary Key
+    ,SecurityCode blob(128)
+    ,ExpirationDate blob(128)
+    ,BillingAddress blob(128)
+    ,Postcode blob(128)
+    ,UserID integer
+    ,FOREIGN KEY(UserID) REFERENCES UsersAndPasswords(UserID))""")#() next to text is length of the inputs in bytes and for text data type it is number of characters
 
     Screenmanager.add_widget(PaymentScreen(name="PaymentScreen"))#adds each screen to screenmanager to so they can be controlled
     Screenmanager.add_widget(ViewBasket(name="ViewBasket"))
     Screenmanager.add_widget(Shopfront(name="Shopfront"))
     Screenmanager.add_widget(Login(name="Login"))
     Screenmanager.current="Login"
-    class PaperApp(App):#app class contains screenmanager
+
+  
+    class PaperApp(App):#app class contains screenmanager  
         def build(self):
             return Screenmanager 
-
     PaperApp().run()
+    cursor.execute("Drop Table Basket")
 
-
-    cursor.execute("Drop table Basket;")#because basket is temporary delete table at end
     cursor.close()
-
 main()
